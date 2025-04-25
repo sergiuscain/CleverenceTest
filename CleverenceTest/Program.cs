9973028b6e272121790e8649edacc482c1dc3c8d
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-04-25 10:11:48 +0300

    Add project files.

---
+++ CleverenceTest/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverenceTest
+{
+    public class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите команду:\n1 - Сжатие текста\n2 - Привод лог файлов к единому формату.\nexit - выход.");
+                string cmd = Console.ReadLine();
+                if (cmd == "1")
+                {
+                    Console.WriteLine("Введите строку для сжатия!");
+                    string line = Console.ReadLine();
+                    var compressionResult = Task1.Compression(line);
+                    Console.WriteLine(compressionResult);
+                    Console.WriteLine("Нажмите любую клавишу для возврата в меню");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                if (cmd == "exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ CleverenceTest/Task1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleverenceTest
+{
+    public static class Task1
+    {
+        public static string Compression(string input)
+        {
+            input += " ";
+            string output = string.Empty;
+            int count = 1;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                if (input[i] == input[i+1])
+                    count++;
+                else
+                {
+                    if (count < 2)
+                        output += input[i];
+                    else
+                    {
+                        output += input[i];
+                        output += count;
+                        count = 1;
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
