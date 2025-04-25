31764b7b0130b7ea4d234fa909a19c9d140db821
sergiussmithe@gmail.com <sergiussmithe@gmail.com>
2025-04-25 10:49:40 +0300

    Задание 2

---
+++ CleverenceTest/Server.cs
@@ -0,0 +1,38 @@
+﻿
+public static class Server
+{
+    private static int count;
+    private static ReaderWriterLockSlim rwLock = new ReaderWriterLockSlim();
+
+    // Метод для чтения значения count
+    public static int GetCount()
+    {
+        // Получаем блокировку для чтения
+        rwLock.EnterReadLock();
+        try
+        {
+            return count;
+        }
+        finally
+        {
+            // Освобождаем блокировку
+            rwLock.ExitReadLock();
+        }
+    }
+
+    // Метод для добавления значения к count
+    public static void AddToCount(int value)
+    {
+        // Получаем блокировку для записи
+        rwLock.EnterWriteLock();
+        try
+        {
+            count += value;
+        }
+        finally
+        {
+            // Освобождаем блокировку
+            rwLock.ExitWriteLock(); 
+        }
+    }
+}
